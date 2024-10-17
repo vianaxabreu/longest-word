8b63dff9d0157d84ecbae048fc27b8862675243c
Alexsandro Viana de Abreu <viana.abreu@outlook.com>
2024-10-17 10:17:43 +0000

    initial commit

---
+++ longest_word/game.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import string
+import random
+
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy()  # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
