35ee26cb1251711d6b0c663ef8ae1ab13da03b64
Alex Viana <viana.abreu@outlook.com>
2025-02-12 21:23:53 +0000

    TDD: Check that attempt exists in the English dictionary

--- tests/test_game.py
+++ tests/test_game.py
@@ -30,3 +30,8 @@ class TestGame:
         assert new_game.grid == list(
             "KWEUEAKRZ"
         )  # Make sure the grid remained untouched
+    def test_unknown_word_is_invalid(self):
+        """A word that is not in the English dictionary should not be valid"""
+        new_game = Game()
+        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+        assert new_game.is_valid('FEUN') is False
